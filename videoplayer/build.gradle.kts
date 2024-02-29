07031ebad41109db27993c4768b4638bbd9fe112
Lanlinju <lanlinju@outlook.com>
2024-02-29 20:28:20 +0800

    add: add relevant exoplayer dependencies

--- videoplayer/build.gradle.kts
+++ videoplayer/build.gradle.kts
@@ -34,10 +34,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation(api("com.google.android.exoplayer:exoplayer-core:2.16.1")!!)
+    implementation(api("com.google.android.exoplayer:exoplayer-hls:2.16.1")!!)
 }
