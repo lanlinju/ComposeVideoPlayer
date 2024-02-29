2e6b6c834810653624b3a1cb7057b06079d61e00
Lanlinju <lanlinju@outlook.com>
2024-02-29 23:37:40 +0800

    feat: implement the function of the videoplayer module

---
+++ videoplayer/src/main/java/com/sakura/videoplayer/VideoPlayerControl.kt
@@ -0,0 +1,306 @@
+package com.sakura.videoplayer
+
+import android.content.res.Configuration
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.imherrera.videoplayer.icons.Fullscreen
+import com.imherrera.videoplayer.icons.FullscreenExit
+import com.sakura.videoplayer.component.Slider
+import com.sakura.videoplayer.icons.ArrowBackIos
+import com.sakura.videoplayer.icons.Pause
+import kotlin.time.Duration.Companion.milliseconds
+
+@Composable
+fun VideoPlayerControl(
+    state: VideoPlayerState,
+    title: String,
+    subtitle: String? = null,
+    background: Color = Color.Black.copy(0.25f),
+    contentColor: Color = Color.White,
+    progressLineColor: Color = MaterialTheme.colorScheme.inversePrimary,
+    onBackClick: () -> Unit = {},
+    onOptionsClick: (() -> Unit)? = null,
+) {
+    CompositionLocalProvider(LocalContentColor provides contentColor) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(background)
+                .padding(
+                    start = 8.dp + if (
+                        LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+                    ) {
+                        WindowInsets.displayCutout
+                            .asPaddingValues()
+                            .calculateLeftPadding(LayoutDirection.Ltr)
+                    } else 0.dp, end = 28.dp, top = 20.dp
+                )
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                if (!state.isSeeking.value) {
+                    ControlHeader(
+                        modifier = Modifier.fillMaxWidth(),
+                        title = title,
+                        subtitle = subtitle,
+                        onOptionsClick = onOptionsClick,
+                        onBackClick = onBackClick
+                    )
+                }
+
+                Spacer(Modifier.size(1.dp))
+
+                TimelineControl(
+                    modifier = Modifier.fillMaxWidth(),
+                    progressLineColor = progressLineColor,
+                    isFullScreen = state.isFullscreen.value,
+                    videoDurationMs = state.videoDurationMs.value,
+                    videoPositionMs = state.videoPositionMs.value,
+                    videoProgress = state.videoProgress.value,
+                    control = state.control,
+                    isSeeking = state.isSeeking.value,
+                    isPlaying = state.isPlaying.value,
+                    onFullScreenToggle = { state.control.setFullscreen(!state.isFullscreen.value) },
+                    onDragSlider = { state.onSeeking(it) },
+                    onDragSliderFinished = { state.onSeeked() },
+                    speedText = state.speedText.value,
+                    resizeText = state.resizeText.value,
+                    onSpeedClick = { state.showSpeedUi() },
+                    onResizeClick = { state.showResizeUi() },
+                    onEpisodeClick = { state.showEpisodeUi() }
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun ControlHeader(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String?,
+    onBackClick: (() -> Unit)?,
+    onOptionsClick: (() -> Unit)? = null,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(modifier = Modifier
+            .size(BigIconButtonSize)
+            .padding(10.dp), onClick = { onBackClick?.invoke() }) {
+            Icon(imageVector = Icons.Rounded.ArrowBackIos, contentDescription = null)
+        }
+        Column(
+            modifier = Modifier.weight(1F),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                color = LocalContentColor.current,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            if (subtitle != null) {
+                Text(
+                    text = subtitle,
+                    color = LocalContentColor.current.copy(0.80f),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+        if (onOptionsClick != null) {
+            AdaptiveIconButton(
+                modifier = Modifier.size(MediumIconButtonSize),
+                onClick = onOptionsClick
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.MoreVert,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun TimelineControl(
+    modifier: Modifier,
+    progressLineColor: Color,
+    isFullScreen: Boolean,
+    videoDurationMs: Long,
+    videoPositionMs: Long,
+    videoProgress: Float,
+    isSeeking: Boolean,
+    isPlaying: Boolean,
+    control: VideoPlayerControl,
+    onDragSlider: (Float) -> Unit,
+    onDragSliderFinished: () -> Unit,
+    onFullScreenToggle: () -> Unit,
+    speedText: String,
+    resizeText: String,
+    onSpeedClick: () -> Unit,
+    onResizeClick: () -> Unit,
+    onEpisodeClick: () -> Unit,
+) {
+    val timestamp = remember(videoDurationMs, videoPositionMs.milliseconds.inWholeSeconds) {
+        prettyVideoTimestamp(videoPositionMs.milliseconds, videoDurationMs.milliseconds)
+    }
+    Column(
+        modifier = modifier
+    ) {
+        if (!isSeeking)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = timestamp, style = MaterialTheme.typography.bodySmall)
+                Spacer(modifier = Modifier.weight(1.0f))
+                AdaptiveIconButton(
+                    modifier = Modifier.size(SmallIconButtonSize),
+                    onClick = onFullScreenToggle
+                ) {
+                    Icon(
+                        imageVector = if (isFullScreen) Icons.Rounded.FullscreenExit else Icons.Rounded.Fullscreen,
+                        contentDescription = null
+                    )
+                }
+
+            }
+
+        Slider(
+            value = if (videoProgress.isNaN()) 0f else videoProgress,
+            onValueChange = onDragSlider,
+            onValueChangeFinished = onDragSliderFinished,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp),
+            isSeeking = isSeeking,
+            color = progressLineColor,
+            trackColor = Color.LightGray
+        )
+
+        if (isSeeking) {
+            Spacer(modifier = Modifier.size(MediumIconButtonSize))
+        } else {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AdaptiveIconButton(
+                    modifier = Modifier.size(MediumIconButtonSize),
+                    onClick = { if (isPlaying) control.pause() else control.play() }
+                ) {
+                    Icon(
+                        modifier = Modifier.fillMaxSize(),
+                        imageVector = if (isPlaying) Icons.Rounded.Pause else Icons.Rounded.PlayArrow,
+                        contentDescription = null
+                    )
+                }
+
+                Row(horizontalArrangement = Arrangement.spacedBy(24.dp)) {
+                    Text(
+                        text = "选集",
+                        modifier = Modifier.clickable { onEpisodeClick() },
+                        color = LocalContentColor.current,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+
+                    Text(
+                        text = speedText,
+                        modifier = Modifier.clickable { onSpeedClick() },
+                        color = LocalContentColor.current,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+
+                    Text(
+                        text = resizeText,
+                        modifier = Modifier.clickable { onResizeClick() },
+                        color = LocalContentColor.current,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+
+            }
+
+        }
+    }
+}
+
+/**
+ * Allow the button to be any size instead of constraining it to 48dp
+ * **/
+@Composable
+private fun AdaptiveIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                indication = LocalIndication.current
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
+
+private val BigIconButtonSize = 52.dp
+private val MediumIconButtonSize = 42.dp
+private val SmallIconButtonSize = 32.dp
---
+++ videoplayer/src/main/java/com/sakura/videoplayer/VideoPlayerState.kt
@@ -0,0 +1,398 @@
+package com.sakura.videoplayer
+
+import android.app.Activity
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.view.Window
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.video.VideoSize
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Build and remember default implementation of [VideoPlayerState]
+ *
+ * @param hideControllerAfterMs time after which [VideoPlayerState.isControlUiVisible] will be set to false.
+ * interactions with [VideoPlayerState.control] will reset the internal counter.
+ * if null is provided the controller wont be hidden until [VideoPlayerState.hideControlUi] is called again
+ * @param videoPositionPollInterval interval on which the [VideoPlayerState.videoPositionMs] will be updated,
+ * you can set a lower number to update the ui faster though it will consume more cpu resources.
+ * Take in consideration that this value is updated only when [VideoPlayerState.isControlUiVisible] is set to true,
+ * if you need to get the last value use [ExoPlayer.getCurrentPosition].
+ * @param coroutineScope this scope will be used to poll for [VideoPlayerState.videoPositionMs] updates
+ * @param context used to build an [ExoPlayer] instance
+ * @param config you can use this to configure [ExoPlayer]
+ * */
+@Composable
+fun rememberVideoPlayerState(
+    hideControllerAfterMs: Long = 6000,
+    videoPositionPollInterval: Long = 500,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    context: Context = LocalContext.current,
+    config: ExoPlayer.Builder.() -> Unit = {
+        setSeekForwardIncrementMs(10 * 1000)
+        setSeekBackIncrementMs(10 * 1000)
+    }
+): VideoPlayerState = remember {
+    VideoPlayerStateImpl(
+        player = ExoPlayer.Builder(context).apply(config).build(),
+        audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager,
+        window = (context as Activity).window,
+        coroutineScope = coroutineScope,
+        hideControllerAfterMs = hideControllerAfterMs,
+        videoPositionPollInterval = videoPositionPollInterval
+    ).also {
+        it.player.addListener(it)
+    }
+}
+
+class VideoPlayerStateImpl(
+    override val player: ExoPlayer,
+    override val audioManager: AudioManager,
+    override val window: Window,
+    private val coroutineScope: CoroutineScope,
+    private val hideControllerAfterMs: Long,
+    private val videoPositionPollInterval: Long,
+) : VideoPlayerState, Player.Listener {
+    override val videoSize = mutableStateOf(player.videoSize)
+    override val videoResizeMode = mutableStateOf(ResizeMode.Fit)
+    override val videoPositionMs = mutableStateOf(0L)
+    override val videoDurationMs = mutableStateOf(0L)
+
+    override val isFullscreen = mutableStateOf(true)
+    override val isPlaying = mutableStateOf(player.isPlaying)
+    override val isLoading = mutableStateOf(true)
+    override val isEnded = mutableStateOf(false)
+    override val isError = mutableStateOf(false)
+    override val playerState = mutableStateOf(player.playbackState)
+
+    override val isSeeking = mutableStateOf(false)
+    override val isLongPress = mutableStateOf(false)
+    override val isChangingVolume = mutableStateOf(false)
+    override val isChangingBrightness = mutableStateOf(false)
+
+    override val videoProgress = mutableStateOf(0F)
+    override val volumeBrightnessProgress = mutableStateOf(0F)
+
+    override val speedText = mutableStateOf("倍速")
+    override val resizeText = mutableStateOf("适应")
+
+    override val isOptionsUiVisible = mutableStateOf(false)
+    override val isControlUiVisible = mutableStateOf(false)
+    override val isSpeedUiVisible = mutableStateOf(false)
+    override val isResizeUiVisible = mutableStateOf(false)
+    override val isEpisodeUiVisible = mutableStateOf(false)
+
+    override val onSeeking: (Float) -> Unit
+        get() = {
+            controlUiLastInteractionMs = 0
+            isSeeking.value = true
+            isEnded.value = false
+            if (!isControlUiVisible.value) showControlUi()
+            this.videoProgress.value = it
+        }
+    override val onSeeked: () -> Unit
+        get() = {
+            isSeeking.value = false
+            player.seekTo((player.duration * videoProgress.value).toLong())
+        }
+
+    override fun onChangeVolume(value: Float) {
+        isChangingVolume.value = true
+        volumeBrightnessProgress.value = value
+    }
+
+    override fun onChangeBrightness(value: Float) {
+        isChangingBrightness.value = true
+        volumeBrightnessProgress.value = value
+    }
+
+    override fun onChanged() {
+        isChangingVolume.value = false
+        isChangingBrightness.value = false
+    }
+
+    override fun onLongPress() {
+        isLongPress.value = true
+        val currentSpeed = player.playbackParameters.speed
+        control.setPlaybackSpeed(currentSpeed * 2)
+    }
+
+    override fun onDisLongPress() {
+        isLongPress.value = false
+        val currentSpeed = player.playbackParameters.speed
+        control.setPlaybackSpeed(currentSpeed.div(2))
+    }
+
+    override val control = object : VideoPlayerControl {
+        override fun play() {
+            controlUiLastInteractionMs = 0
+            player.play()
+        }
+
+        override fun pause() {
+            controlUiLastInteractionMs = 0
+            player.pause()
+        }
+
+        override fun forward() {
+            controlUiLastInteractionMs = 0
+            player.seekForward()
+        }
+
+        override fun rewind() {
+            controlUiLastInteractionMs = 0
+            player.seekBack()
+        }
+
+        override fun setFullscreen(value: Boolean) {
+            controlUiLastInteractionMs = 0
+            isFullscreen.value = value
+        }
+
+        override fun setVideoResize(mode: ResizeMode) {
+            controlUiLastInteractionMs = 0
+            videoResizeMode.value = mode
+        }
+
+        override fun setPlaybackSpeed(speed: Float) {
+            player.setPlaybackSpeed(speed)
+        }
+    }
+
+    private var pollVideoPositionJob: Job? = null
+    private var controlUiLastInteractionMs = 0L
+
+    override fun hideControlUi() {
+        controlUiLastInteractionMs = 0
+        isControlUiVisible.value = false
+        pollVideoPositionJob?.cancel()
+        pollVideoPositionJob = null
+    }
+
+    override fun showControlUi() {
+        isControlUiVisible.value = true
+        pollVideoPositionJob?.cancel()
+        pollVideoPositionJob = coroutineScope.launch {
+            while (true) {
+                if (videoDurationMs.value > 0) {
+                    videoPositionMs.value = player.currentPosition
+                    if (!isSeeking.value)
+                        videoProgress.value =
+                            videoPositionMs.value / videoDurationMs.value.toFloat()
+                }
+                controlUiLastInteractionMs += videoPositionPollInterval
+
+                delay(videoPositionPollInterval)
+                if (controlUiLastInteractionMs >= hideControllerAfterMs) {
+                    hideControlUi()
+                    break
+                }
+            }
+        }
+    }
+
+    override fun setSpeedText(text: String) {
+        speedText.value = text
+    }
+
+    override fun setResizeText(text: String) {
+        resizeText.value = text
+    }
+
+    override fun hideOptionsUi() {
+        isOptionsUiVisible.value = true
+    }
+
+    override fun showOptionsUi() {
+        isOptionsUiVisible.value = false
+    }
+
+    override fun showSpeedUi() {
+        hideControlUi()
+        isSpeedUiVisible.value = true
+    }
+
+    override fun hideSpeedUi() {
+        isSpeedUiVisible.value = false
+    }
+
+    override fun showResizeUi() {
+        hideControlUi()
+        isResizeUiVisible.value = true
+    }
+
+    override fun hideResizeUi() {
+        isResizeUiVisible.value = false
+    }
+
+    override fun showEpisodeUi() {
+        hideControlUi()
+        isEpisodeUiVisible.value = true
+    }
+
+    override fun hideEpisodeUi() {
+        isEpisodeUiVisible.value = false
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        this.isPlaying.value = isPlaying
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        if (playbackState == Player.STATE_READY) videoDurationMs.value = player.duration
+        this.playerState.value = playbackState
+        when (playbackState) {
+            Player.STATE_IDLE -> Unit
+            Player.STATE_BUFFERING -> isLoading.value = true
+            Player.STATE_READY -> isLoading.value = false
+            Player.STATE_ENDED -> isEnded.value = true
+        }
+    }
+
+    private val audioFocusChangeListener = AudioManager.OnAudioFocusChangeListener { focusChange ->
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                control.play() // 重新获得焦点，恢复播放
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS -> {
+                control.pause() // Permanent loss of audio focus，Pause playback immediately
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+                control.pause() // 暂时失去音频焦点，暂停播放
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+                // 暂时失去音频焦点，但可以继续播放，不过需要降低音量(系统默认降低音量)
+            }
+        }
+    }
+
+    private val focusRequest =
+        AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT).run {
+            setAudioAttributes(AudioAttributes.Builder().run {
+                setUsage(AudioAttributes.USAGE_MEDIA)
+                setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                build()
+            })
+            setAcceptsDelayedFocusGain(true)
+            setOnAudioFocusChangeListener(audioFocusChangeListener)
+            build()
+        }
+
+    override fun onVideoSizeChanged(videoSize: VideoSize) {
+        this.videoSize.value = videoSize
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        isError.value = true
+    }
+
+    override fun onRenderedFirstFrame() {
+        isError.value = false
+        isEnded.value = false
+    }
+
+    override fun requestAudioFocus() {
+        audioManager.requestAudioFocus(focusRequest)
+    }
+
+    override fun abandonAudioFocus() {
+        audioManager.abandonAudioFocusRequest(focusRequest)
+    }
+
+}
+
+
+interface VideoPlayerState {
+    val player: ExoPlayer
+    val audioManager: AudioManager
+    val window: Window
+
+    val videoSize: State<VideoSize>
+    val videoResizeMode: State<ResizeMode>
+    val videoPositionMs: State<Long>
+    val videoDurationMs: State<Long>
+
+    val isFullscreen: State<Boolean>
+    val isPlaying: State<Boolean>
+    val isLoading: State<Boolean>
+    val isEnded: State<Boolean>
+    val isError: State<Boolean>
+    val playerState: State<Int>
+
+    val isSeeking: State<Boolean>
+    val isLongPress: State<Boolean>
+    val isChangingVolume: State<Boolean>
+    val isChangingBrightness: State<Boolean>
+    val videoProgress: State<Float> /*0f - 1f*/
+    val volumeBrightnessProgress: State<Float>
+
+    val onSeeking: (dragProcess: Float) -> Unit
+    val onSeeked: () -> Unit
+
+    val speedText: State<String>
+    val resizeText: State<String>
+
+    val isOptionsUiVisible: State<Boolean>
+    val isControlUiVisible: State<Boolean>
+    val isSpeedUiVisible: State<Boolean>
+    val isResizeUiVisible: State<Boolean>
+    val isEpisodeUiVisible: State<Boolean>
+    val control: VideoPlayerControl
+
+    fun onChangeVolume(value: Float)
+    fun onChangeBrightness(value: Float)
+    fun onChanged()
+
+    fun onLongPress()
+    fun onDisLongPress()
+
+    fun requestAudioFocus()
+    fun abandonAudioFocus()
+
+    fun setSpeedText(text: String)
+    fun setResizeText(text: String)
+
+    fun hideOptionsUi()
+    fun showOptionsUi()
+
+    fun hideControlUi()
+    fun showControlUi()
+
+    fun showSpeedUi()
+    fun hideSpeedUi()
+
+    fun showResizeUi()
+    fun hideResizeUi()
+
+    fun showEpisodeUi()
+    fun hideEpisodeUi()
+}
+
+interface VideoPlayerControl {
+    fun play()
+    fun pause()
+
+    fun forward()
+    fun rewind()
+
+    fun setFullscreen(value: Boolean)
+    fun setVideoResize(mode: ResizeMode)
+    fun setPlaybackSpeed(speed: Float)
+
+}
